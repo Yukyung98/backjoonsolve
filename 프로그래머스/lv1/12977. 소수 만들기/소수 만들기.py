dc0b3d01310946903a3280f69cadb579a2e6afec
Yukyung98 <68256872+Yukyung98@users.noreply.github.com>
2023-09-24 16:55:03 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+def isprime(num):
+    if num==0 or num==1:
+        return False
+    else:
+        for n in range(2,(num//2)+1):
+            if num%n == 0:
+                return False
+        return True
+    
+def solution(nums):
+    answer = 0
+    cmb = list(combinations(nums,3))
+    for arr in cmb:
+        if isprime(sum(arr)):
+            answer += 1      
+    return answer
