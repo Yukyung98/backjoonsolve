faabe098144896726882e7e40cdbd9faef6a2332
Yukyung98 <68256872+Yukyung98@users.noreply.github.com>
2024-09-06 23:16:30 +0900

    [level 3] Title: 네트워크, Time: 0.83 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,23 +1,25 @@
 from collections import deque
 
-def bfs(start, computers, visited):
+def bfs(start, visited, computers, n):
     queue = deque([start])
     visited[start] = True
     
     while queue:
         node = queue.popleft()
-        for i in range(len(computers)):
-            if not visited[i] and computers[node][i] == 1:
+        
+        for i in range(n):
+            # 연결되어 있고, 아직 방문하지 않은 컴퓨터 탐색
+            if computers[node][i] == 1 and not visited[i]:
                 visited[i] = True
                 queue.append(i)
 
 def solution(n, computers):
     visited = [False] * n
-    network_count = 0
+    count = 0
     
     for i in range(n):
         if not visited[i]:
-            bfs(i, computers, visited)
-            network_count += 1
-            
-    return network_count
+            bfs(i, visited, computers, n)
+            count += 1  # 새로운 네트워크 발견 시 카운트 증가
+    
+    return count
