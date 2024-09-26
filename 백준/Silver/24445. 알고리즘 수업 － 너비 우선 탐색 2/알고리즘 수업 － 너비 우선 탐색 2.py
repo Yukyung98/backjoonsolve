e84853a4cdecb51439b42fda4f636bb592284b09
Yukyung98 <68256872+Yukyung98@users.noreply.github.com>
2024-09-26 14:19:14 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 492 ms, Memory: 58592 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+    
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [0]*(N+1)
+cnt = 1
+for _ in range(M):
+    u, v  = map(int,input().split())
+    if 1 <= u <= N and 1 <= v <= N:  
+        graph[u].append(v)
+        graph[v].append(u)
+def bfs(startNode):
+    global cnt
+    queue= deque([R])
+    visited[startNode] = cnt
+    cnt +=1
+    while queue:
+        node = queue.popleft()
+        graph[node].sort(reverse=True)
+        for i in graph[node]:
+            if visited[i] == 0:
+                visited[i] = cnt
+                cnt+=1
+                queue.append(i)  
+# 시작 정점이 범위 안에 있는지 확인
+if 1 <= R <= N:
+    bfs(R)
+for i in range(1,N+1):
+    print(visited[i])
