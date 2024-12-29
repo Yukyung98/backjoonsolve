42ca35041436a65ad73d18bb651c9d7af05b0525
Yukyung98 <68256872+Yukyung98@users.noreply.github.com>
2024-12-30 00:22:55 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,18 @@
+def solution(tickets):
+    for ticket in tickets:
+        ticket.append(0)                                 #visited
+    length = len(tickets)+1                              #경로길이
+    tickets.sort(key = lambda x:x[1])
+    t = ["ICN"]                                          #경로저장리스트
+    def dfs(port):                                       #깊이우선탐색
+        for ticket in tickets:               
+            if ticket[0] == port and ticket[2] == 0:     #ticket이 루트에 해당하면서, 방문하지 않은경우
+                t.append(ticket[1])                      #경로 저장
+                ticket[2] = 1                            #visited 체크
+                if length == len(t) or dfs(ticket[1]):   #경로가 꽉찼으면 return t 해주고, 그렇지 않으면 다음탐색
+                    return t
+                ticket[2] = 0                            #탐색했는데 경로 안꽉찼으면 visited = 0
+                t.pop()                                  #경로 빼내기
+    answer = dfs("ICN")
+
+    return answer
