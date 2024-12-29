371f5358f7aaa736b26990ec05aadabb44bd3412
Yukyung98 <68256872+Yukyung98@users.noreply.github.com>
2024-12-29 23:22:41 +0900

    [level 3] Title: 네트워크, Time: 0.64 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,21 +1,18 @@
 from collections import deque
-def bfs(start,computers,N,visited):
-    queue = deque()
-    queue.append(start)
+def bfs(startNode,visitedGraph,computers):
+    queue = deque([startNode])
     
     while queue:
-        node = queue.popleft()
-        visited[node] = 1
-        for i in range(N):
-            if computers[node][i] == 1 and visited[i] != 1 :
-                visited[i] = 1
-                queue.append(i)
-            
+        x = queue.popleft()
+        for node in range(len(computers[x])):
+            if computers[x][node] == 1 and not visitedGraph[node]:
+                visitedGraph[node] = 1
+                queue.append(node)
 def solution(n, computers):
-    visited =[0]*n
+    visited = [0]*n
     answer = 0
     for i in range(n):
-        if visited[i] == 0:  # 아직 방문하지 않은 노드에 대해 BFS 호출
-            bfs(i, computers, n, visited)
-            answer += 1
+        if visited[i] == 0:
+            bfs(i,visited,computers)
+            answer+=1
     return answer
