c05df3f87571f55d2688d7c902df23d7aa749efc
Yukyung98 <68256872+Yukyung98@users.noreply.github.com>
2024-12-31 00:51:36 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 548 ms, Memory: 68064 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -1,32 +1,37 @@
 import sys
 from collections import deque
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10**6)  # 재귀 깊이 설정
 input = sys.stdin.readline
-    
+
+# 입력 받기
 N, M, R = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-visited = [0]*(N+1)
-cnt = 1
+graph = {i: [] for i in range(1, N + 1)}
+
+# 간선 입력 받기
 for _ in range(M):
-    u, v  = map(int,input().split())
-    if 1 <= u <= N and 1 <= v <= N:  
-        graph[u].append(v)
-        graph[v].append(u)
-def bfs(startNode):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+# 각 노드의 인접 리스트를 오름차순으로 정렬
+for key in graph:
+    graph[key].sort()
+
+visited = [0] * (N+1)  # 방문 체크 배열
+cnt = 1
+def bfs(v):
     global cnt
-    queue= deque([R])
-    visited[startNode] = cnt
-    cnt +=1
+    queue = deque([R])
+    visited[v] = cnt  # 현재 노드 방문 처리
     while queue:
         node = queue.popleft()
-        graph[node].sort()
-        for i in graph[node]:
-            if visited[i] == 0:
-                visited[i] = cnt
+        for x in graph[node]:
+            if visited[x] == 0:
                 cnt+=1
-                queue.append(i)  
-# 시작 정점이 범위 안에 있는지 확인
-if 1 <= R <= N:
-    bfs(R)
-for i in range(1,N+1):
+                visited[x] = cnt
+                queue.append(x)
+    
+# DFS 실행
+bfs(R)
+for i in range(1,len(visited)):
     print(visited[i])
