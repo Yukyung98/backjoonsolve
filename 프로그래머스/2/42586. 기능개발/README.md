0bd908cc3ae899e102ce833e567191be8e7606c1
Yukyung98 <68256872+Yukyung98@users.noreply.github.com>
2025-01-11 21:19:33 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.02 ms
+메모리: 4.17 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 07일 23:42:21
+2025년 01월 11일 21:19:30
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,25 +1,37 @@
 #include <string>
 #include <vector>
 #include <queue>
-
+#include <iostream>
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     queue<int> q;
-    int size = speeds.size(); // size를 int형으로 따로 저장해 두는 것이 시간적으로 이득
-    for (int i = 0; i < size; ++i)
-        q.push(i);
-    while(!q.empty()){
+    for(int i=0;i<speeds.size();i++){
         int cnt = 0;
-        for(int j = 0; j < size; ++j)
-            progresses[j] += speeds[j]; // progresses에 speeds를 더해주는 과정
-        while(!q.empty() && progresses[q.front()] >= 100){ // 100이 넘어갔을 때 처리
-            ++cnt;
-            q.pop();
+        while(progresses[i]<100){
+            cnt++;
+            progresses[i]+=speeds[i];
         }
-        if(cnt != 0)
-            answer.push_back(cnt);  
+        q.push(cnt);
     }
+    int cnt2 = 0;
+    int max = 0;
+    while (!q.empty()) {
+        int a = q.front();
+        cnt2++;
+        q.pop();
+        if(a>max){
+            max = a;
+        }
+        if(max<q.front()){
+            answer.push_back(cnt2);
+            cnt2 =0;
+        } else if(q.empty()){
+            answer.push_back(cnt2);
+        }
+        
+
+	}
     return answer;
 }
