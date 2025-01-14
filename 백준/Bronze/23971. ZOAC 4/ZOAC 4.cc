4622823b8783d561ee7c121d786290b4a3958063
Yukyung98 <68256872+Yukyung98@users.noreply.github.com>
2025-01-14 22:04:11 +0900

    [Bronze III] Title: ZOAC 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+double  H,W,N,M ; 
+
+int main(){
+    cin >> H>>W>>N>>M;
+ 
+    
+    cout << (long long)ceil(H/(N+1))*(long long)ceil(W/(M+1));
+}
+    
